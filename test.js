3be5a23e7457fb1d4c74a9d843c66774118bc787
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-04-17 12:59:34 +0530

    Updates.

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
+// @flow
+/*  eslint-env node */
 import test from "ava";
+// import React from "react";
+// import { shallow } from "enzyme";
 
-test.todo("fix the API man.");
+test("Babel compiles?", (t) => {
+  const testRestSpread = { test: 1, test2: 2, test3: 3 };
+  const testRestSpread2 = { ...testRestSpread, test4: 4 };
+  t.is(testRestSpread2, { test: 1, test2: 2, test3: 3, test4: 4 });
+});
